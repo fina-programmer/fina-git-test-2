9fb68c990a328ff4ba1f802ee49ba224fe3f00ea
Fina Alfiana <fina.programmer@gmail.com>
2023-11-16 09:00:03 +0700

    Menambahkan list & keterangan

--- index.php
+++ index.php
@@ -7,5 +7,10 @@
 </head>
 <body>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis, ea laborum in soluta at atque optio repellat ab quos, vero ullam!</p>
+    <ul>
+        <li>Satu</li>
+        <li>Dua</li>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum repudiandae neque laborum placeat hic, cum reiciendis odit doloremque et officia ea quo dolorem dignissimos facilis, error omnis tempore adipisci quaerat!</p>
+    </ul>
 </body>
 </html>
